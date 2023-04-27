cb0c2d84488d00f8efcc97d98c71b307b3289390
playcsgo <sam@plum.wine>
2023-04-27 18:39:32 +0800

    revised and call dotenv for PORT

--- app.js
+++ app.js
@@ -1,14 +1,13 @@
 const express = require('express')
 const app = express()
-const PORT = process.env.PORT || 3000
 const exphbs = require('express-handlebars')
 const routes = require('./routes')
 const methodOverride = require('method-override')
 const session = require('express-session')
 const usePassport = require('./config/passport')
 const flash = require('connect-flash')
-
 require('dotenv').config()
+const PORT = process.env.PORT || 3000
 require('./config/mongoose')
 app.use(express.static('public'))
 app.engine('hbs', exphbs.engine({defaultLayout: 'main', extname: 'hbs'}))
